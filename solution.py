fac6715e8d81595266439c24686d29834d016ae7
ALEXfeed1407 <100508764+ALEXfeed1407@users.noreply.github.com>
2023-04-11 18:04:16 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,11 +8,13 @@ def solution(x_success: int,
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
+    from scipy.stats import norm
     a1 = x_success /  x_cnt
     a2 = y_success /y_cnt
-    diff = sqrt((a1 * (1 - a1) / x_cnt) + (a2 * (1-a2) / y_cnt))
+    diff = np.sqrt((a1 * (1 - a1) / x_cnt) + (a2 * (1-a2) / y_cnt))
     z_ = (a2 - a1) / diff
     z_crit = norm.ppf(1-0.02/2)
     print(z_,z_crit)
     decision = z_ > z_crit
+  
     return decision
