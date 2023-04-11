a6715faf29823780d4958f2c2a5e2305708aede2
ALEXfeed1407 <100508764+ALEXfeed1407@users.noreply.github.com>
2023-04-11 18:03:00 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,17 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 288759659 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    a1 = x_success /  x_cnt
+    a2 = y_success /y_cnt
+    diff = sqrt((a1 * (1 - a1) / x_cnt) + (a2 * (1-a2) / y_cnt))
+    z_ = (a2 - a1) / diff
+    z_crit = norm.ppf(1-0.02/2)
+    print(z_,z_crit)
+    decision = z_ > z_crit
+    return decision
